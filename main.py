c8b1a62d7299552b0654f930d695b33109214111
seba <stinoco@fen.uchile.cl>
2023-03-22 17:28:19 -0300

    se agrega funcion

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+def animales(input):
+    
+    '''
+    Como hace el animalito?
+    '''
+    
+    if input.lower() == 'gato':
+        print('miau')
+    if input.lower() == 'perro':
+        print('guau')
+    if input.lower() == 'fox':
+        print('https://www.youtube.com/watch?v=jofNR_WkoCE')
+    else:
+        raise ValueError('animal no reconocido :(')
+
 if __name__ == '__main__':
     print("Ingrese el animalito para saber que dice:")
     animal = input()
-    # agregar funcion
+    animales(animal)
